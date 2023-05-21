0e2d6ef3f9a99fff61b4fdf6e9c4709442493008
roneslav <119205611+roneslav@users.noreply.github.com>
2023-05-21 21:43:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from models.PreciousStone import PreciousStone
-from models.ArtificialPreciousStone import ArtificialPreciousStone
-from managers.StoneManager import StoneManager
-from models.JewelryStone import JewelryStone
-from models.SemiPreciousStone import SemiPreciousStone
+from models.precious_stone import precious_stone
+from models.artificial_precious_stone import artificial_precious_stone
+from managers.stone_manager import stone_manager
+from models.jewelry_stone import jewelry_stone
+from models.semi_precious_stone import semi_precious_stone
 
 
 def main():
     print('All stones:')
-    stone_manager = StoneManager()
-    stone_manager.add_in_list(PreciousStone(55, 4, 25, 'blue', 'square', 'Diamond'))
-    stone_manager.add_in_list(PreciousStone(100, 5, 35, 'green', 'circle', 'Ruby'))
-    stone_manager.add_in_list(ArtificialPreciousStone('BioLab', 5, 20, 'green', 'circle', 'Cubic Zirconia'))
-    stone_manager.add_in_list(ArtificialPreciousStone('DeltaLab', 4, 25, 'pink', 'trapezium', 'Moissanite'))
-    stone_manager.add_in_list(JewelryStone('Pearl', 'red', 5, 'square'))
-    stone_manager.add_in_list(JewelryStone('Opal', 'green', 3, 'circle'))
-    stone_manager.add_in_list(SemiPreciousStone('Amethyst', 'pink', 5, 25, 'circle'))
-    stone_manager.add_in_list(SemiPreciousStone('Aquamarine', 'green', 4, 100, 'trapezium'))
+    stone_manager = stone_manager()
+    stone_manager.add_in_list(precious_stone(55, 4, 25, 'blue', 'square', 'Diamond'))
+    stone_manager.add_in_list(precious_stone(100, 5, 35, 'green', 'circle', 'Ruby'))
+    stone_manager.add_in_list(artificial_precious_stone('BioLab', 5, 20, 'green', 'circle', 'Cubic Zirconia'))
+    stone_manager.add_in_list(artificial_precious_stone('DeltaLab', 4, 25, 'pink', 'trapezium', 'Moissanite'))
+    stone_manager.add_in_list(jewelry_stone('Pearl', 'red', 5, 'square'))
+    stone_manager.add_in_list(jewelry_stone('Opal', 'green', 3, 'circle'))
+    stone_manager.add_in_list(semi_precious_stone('Amethyst', 'pink', 5, 25, 'circle'))
+    stone_manager.add_in_list(semi_precious_stone('Aquamarine', 'green', 4, 100, 'trapezium'))
     for stone in stone_manager.stones:
         print(stone)
 
